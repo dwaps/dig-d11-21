283d5946be9709df0f9546b7916111bf7a0986c7
Michael CORNILLON <ei.dwaps@gmail.com>
2021-10-07 10:57:04 +0200

    tp1: refacto

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const coco = {
-  firstname: "Coco",
-  lastname: "COCO",
-  age: 56,
-  isMan: true,
-  friends: [ "Jaco", "Fernando", "Franco" ],
-  address: {
-    street: "Rue de la Liberté",
-    zip: "55555",
-    city: "Paris"
+/**
+ * CONSIGNE
+ * Extraire le nombre le plus grand d'une liste donnée.
+ * 
+ * 1- L'algorithme fera l'objet d'une fonction.
+ * 2- La liste devra être fournie par l'utilisateur.
+ * 3- Afficher le résultat sur la console du navigateur.
+ */
+
+const app = {
+  init() {
+    document.getElementById("button").onclick = this.reload;
+    const max = this.getListe();
+    if (max) this.displayResult(max);
   },
-  presentation() {
-    console.log("Je m'appelle " + this.firstname);
+  getListe() {
+    const liste = prompt("Saisir une liste de nombres (séparés par un espace)").split(" ");
+    return this.maxOf(liste);
+  },
+  maxOf(liste) {
+    let value = 0;
+    for (let listItem of liste) {
+      if (listItem > value) value = +listItem;
+    }
+    return value;
+  },
+  reload() {
+    location.reload();
+  },
+  displayResult(max) {
+    document.getElementById("result").hidden = false;
+    document.getElementById("output").value = max;
   }
 };
 
-coco.email = "coco@mail.fr";
-
-const jaco = Object.create(coco);
-console.log(jaco);
-
-// coco.presentation();
+app.init();
