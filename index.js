165c6f0c1a8acffd934be37758c1022346871cd6
Michael CORNILLON <ei.dwaps@gmail.com>
2021-10-07 12:10:53 +0200

    tp 1: via champ de saisi

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-/**
- * CONSIGNE
- * Extraire le nombre le plus grand d'une liste donnée.
- * 
- * 1- L'algorithme fera l'objet d'une fonction.
- * 2- La liste devra être fournie par l'utilisateur.
- * 3- Afficher le résultat sur la console du navigateur.
- */
-
 const app = {
   init() {
-    document.getElementById("button").onclick = this.reload;
-    const max = this.getListe();
-    if (max) this.displayResult(max);
+    // DECLARATIONS
+    const btSubmit = document.querySelector("button#bt-submit");
+    const inputTag = document.querySelector("input");
+    this.resultTag = document.querySelector("#result");
+    this.outputTag = document.querySelector("#output");
+    
+    // CONFIGURATION
+    btSubmit.addEventListener('click', () => {
+      const userInput = inputTag.value;
+      const max = this.maxOf(userInput.split(' '));
+      this.displayResult(max);
+      // Reset input
+      inputTag.value = '';
+      inputTag.focus();
+    });
   },
-  getListe() {
-    const liste = prompt("Saisir une liste de nombres (séparés par un espace)").split(" ");
-    return this.maxOf(liste);
-  },
-  maxOf(liste) {
+  maxOf(liste = []) {
     let value = 0;
     for (let listItem of liste) {
       if (listItem > value) value = +listItem;
     }
     return value;
   },
-  reload() {
-    location.reload();
-  },
   displayResult(max) {
-    document.getElementById("result").hidden = false;
-    document.getElementById("output").value = max;
+    this.resultTag.hidden = false;
+    this.outputTag.value = max;
   }
 };
 
