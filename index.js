10c3a3360dfb4bf5a90fe09ec54266165beb3081
Michael CORNILLON <ei.dwaps@gmail.com>
2021-10-07 12:44:48 +0200

    js todolist: création des éléments

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+// DECLARATION
+const todoDiv = document.createElement('div');
+const todoStateInput = document.createElement('input');
+const todoTxtPar = document.createElement('p');
+const deleteBt = document.createElement('button');
 
+console.log(todoDiv);
+console.log(todoStateInput);
+console.log(todoTxtPar);
+console.log(deleteBt);
+
+// <div class="todo">
+//   <input type="checkbox">
+//   <p class="todo-txt">Lorem ipsum dolor sit amet consectetur.</p>
+//   <button class="bt-closed">&times;</button>
+// </div>
