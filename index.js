d6ab80ee9d31727fdaf276b2f6e3ff6e19203246
Michael CORNILLON <ei.dwaps@gmail.com>
2021-10-07 14:07:03 +0200

    tp todolist: fonction createTodo

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-// DECLARATION
-const todoDiv = document.createElement('div');
-const todoStateInput = document.createElement('input');
-const todoTxtPar = document.createElement('p');
-const deleteBt = document.createElement('button');
-
-// CONFIGURATION
-todoDiv.className = 'todo';
-todoStateInput.type = 'checkbox';
-todoTxtPar.className = 'todo-txt';
-todoTxtPar.innerText = "Lorem ipsum dolor sit amet consectetur.";
-deleteBt.className = 'bt-closed';
-deleteBt.innerHTML = '&times;';
-
-// IMBRICATION
-todoDiv.appendChild(todoStateInput);
-todoDiv.appendChild(todoTxtPar);
-todoDiv.appendChild(deleteBt);
-
-// INJECTION DANS LE DOM
-document
+function createTodo() {
+  // DECLARATION
+  const todoDiv = document.createElement('div');
+  const todoStateInput = document.createElement('input');
+  const todoTxtPar = document.createElement('p');
+  const deleteBt = document.createElement('button');
+  
+  // CONFIGURATION
+  todoDiv.className = 'todo';
+  todoStateInput.type = 'checkbox';
+  todoTxtPar.className = 'todo-txt';
+  todoTxtPar.innerText = "Lorem ipsum dolor sit amet consectetur.";
+  deleteBt.className = 'bt-closed';
+  deleteBt.innerHTML = '&times;';
+  
+  // IMBRICATION
+  todoDiv.appendChild(todoStateInput);
+  todoDiv.appendChild(todoTxtPar);
+  todoDiv.appendChild(deleteBt);
+  
+  // INJECTION DANS LE DOM
+  document
   .querySelector('.todos')
   .appendChild(todoDiv);
+}
 
 
-  
-// <div class="todo">
-//   <input type="checkbox">
-//   <p class="todo-txt">Lorem ipsum dolor sit amet consectetur.</p>
-//   <button class="bt-closed">&times;</button>
-// </div>
+createTodo();
+createTodo();
+createTodo();
