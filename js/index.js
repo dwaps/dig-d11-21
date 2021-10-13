73e06c83634ddd0391a50a3573f084f150715aaf
INGOLD <fogold07@gmail.com>
2021-10-13 13:07:09 +0200

    initialisation du TP + squelette maquette

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import { applyBgBlur } from './utils.js';
-import { App } from './app.js';
-import { Router } from './router.js';
-
-new App();
-Router.run();
-
-applyBgBlur();
-window.addEventListener('resize', applyBgBlur);
