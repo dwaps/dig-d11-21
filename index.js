42418e4ee0d8ccb4ae513482c38aada3fbeaeed9
Steeve <steeve.dombald.pro@outlook.com>
2021-10-17 16:46:17 +0200

    responsive design done

---
+++ index.js
@@ -0,0 +1,18 @@
+// $(function(){ 
+//     var navMain = $(".offcanvas");
+//     navMain.on("click", "a", null, function () {
+//         navMain.collapse('hide');
+//     });
+// });
+
+
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('offcanvasNavbar')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+
+
+
