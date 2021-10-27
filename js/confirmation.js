30cc175a4b88e79504f8fec22aeed1eb8e12ecb1
Celia <80826991+celiawork@users.noreply.github.com>
2021-10-27 23:19:27 +0200

    Update confirmation.js

--- js/confirmation.js
+++ js/confirmation.js
@@ -12,7 +12,7 @@ export function displayInformation() {
 <p>Nom : ${customers[0].lastname}</p>
 <p>Prénom : ${customers[0].firstname}</p>
 <p>Email : ${customers[0].email}</p>
-<p>Phone : ${customers[0].phone}</p>
+<p>Telephone : ${customers[0].phone}</p>
 
 <p>Adresse : ${address[0].numStreet} ${address[0].nameStreet} ${address[0].zipCode} ${address[0].city}</p>
 
