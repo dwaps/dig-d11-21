b1da1266fb66651c4f40c161e2cc3934d52c01c3
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-14 14:58:40 +0100

    7/ UserService --> findAll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,6 +20,7 @@ import { TodosComponent } from './components/todos/todos.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = environment.urlApi + '/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl);
+  }
 }
