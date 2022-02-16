49ae017b97206e7ed01ee59f553baf356f2cc699
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-16 11:51:53 +0100

    récupération référence à contenu projeté

--- src/app/components/compo2/compo2.component.ts
+++ src/app/components/compo2/compo2.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-compo2',
   templateUrl: './compo2.component.html',
   styleUrls: ['./compo2.component.css'],
 })
-export class Compo2Component {
+export class Compo2Component implements AfterContentInit {
 
-  test = 'coco';
+  @ContentChild('projectedTitle')
+  title!: ElementRef<HTMLHeadingElement>;
 
-  double(a: number) {
-    return a*2;
+  ngAfterContentInit() {
+    console.log(this.title?.nativeElement?.innerText);
   }
 
 }
