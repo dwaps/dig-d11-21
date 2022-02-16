cf38652c9f38b45568964e8c244df82dd5fbd145
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-16 11:30:24 +0100

    projection de contenu

--- src/app/components/compo1/compo1.component.ts
+++ src/app/components/compo1/compo1.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren, ViewEncapsulation } from '@angular/core';
-import { Compo2Component } from '../compo2/compo2.component';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-compo1',
@@ -7,29 +6,6 @@ import { Compo2Component } from '../compo2/compo2.component';
   styleUrls: ['./compo1.component.css'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class Compo1Component implements OnInit, AfterViewInit {
-
-  @ViewChildren('myPar')
-  myPar!: QueryList<ElementRef<HTMLElement>>;
-
-  @ViewChildren(Compo2Component)
-  children!: QueryList<Compo2Component>;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit() {
-  }
-
-  showTxt() {
-    let count = 0;
-    this.children.forEach((c2: Compo2Component) => {
-      console.log(
-        c2.double(++count)
-      );
-    })
-  }
-
+export class Compo1Component {
+  
 }
