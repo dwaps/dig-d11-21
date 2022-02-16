20518fbbf991dcc0600f8e33e3713aa33af92475
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-16 10:36:28 +0100

    manipulation ref loc&ales depuis le ts

--- src/app/components/compo1/compo1.component.ts
+++ src/app/components/compo1/compo1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Compo2Component } from '../compo2/compo2.component';
 
 @Component({
   selector: 'app-compo1',
@@ -6,11 +7,21 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./compo1.component.css'],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class Compo1Component implements OnInit {
+export class Compo1Component implements OnInit, AfterViewInit {
+
+  @ViewChild('title')
+  title!: ElementRef<HTMLElement>;
+
+  @ViewChild('child')
+  compo2!: Compo2Component;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.title.nativeElement.innerText = 'Double de 4 = ' + this.compo2.double(4);
   }
 
 }
