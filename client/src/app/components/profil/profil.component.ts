590a58e6436ba8883d14fdb1bf0c7b25ce1887a6
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-22 16:43:44 +0100

    update profil component

--- client/src/app/components/profil/profil.component.ts
+++ client/src/app/components/profil/profil.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss']
 })
-export class ProfilComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProfilComponent {
+  public user$ = this.authService.user$;
+  constructor(private authService: AuthService) {}
 }
