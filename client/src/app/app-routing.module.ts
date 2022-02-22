b72a14edef9c5da60ca7bd3eb9f22167d70fb3fb
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-22 16:06:31 +0100

    garde d'authentification (check if user is conneted)

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfilComponent } from './components/profil/profil.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 const routes: Routes = [
   {
@@ -20,6 +21,7 @@ const routes: Routes = [
   },
   {
     path: 'profil',
+    canActivate: [AuthGuard],
     component: ProfilComponent
   },
   { path: '**', redirectTo: '' }
---
+++ client/src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { first, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+  canActivate() {
+    return this.authService.isLoggedIn$.pipe(
+      first(),
+      tap((isLoggedIn: boolean) => {
+        console.log(isLoggedIn);
+        if (!isLoggedIn) {
+          this.router.navigateByUrl('/login')
+        }
+      })
+    );
+  }
+
+}
