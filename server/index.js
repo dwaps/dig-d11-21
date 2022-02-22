82d5a2890c52ec558daf64824761cd22fc164fd4
Michael CORNILLON <ei.dwaps@gmail.com>
2022-02-22 13:53:21 +0100

    server: test routes login/signup

--- server/index.js
+++ server/index.js
@@ -8,9 +8,20 @@ const { resolve } = require('path');
 const app = express();
 
 // CONFIGURATIONS
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(resolve('..', 'public')));
+app.use(cookieParser());
 
 // API ROUTES
+const router = express.Router();
+router.post('/signup', (req, res) => {
+  res.send('signup');
+});
+router.post('/login', (req, res) => {
+  res.send('login');
+});
+app.use('/api/auth', router);
 
 app.get('*', (req, res) => {
   res.sendFile(resolve('..', 'public', 'index.html'));
