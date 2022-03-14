27e063c10b118e83564b317f111a67d754d46abf
Michael CORNILLON <ei.dwaps@gmail.com>
2022-03-14 16:52:46 +0100

    categories: raw list

--- src/app/tabs/categories/categories.page.ts
+++ src/app/tabs/categories/categories.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.page.scss'],
 })
 export class CategoriesPage implements OnInit {
+  categories: string[] = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology',
+  ];
+  selected: string = this.categories[0];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  segmentChanged(e: CustomEvent<IonSegment>) {
+    console.log(e.detail.value);
   }
 
 }
